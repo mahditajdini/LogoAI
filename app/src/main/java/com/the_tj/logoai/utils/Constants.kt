6bd4233f35cc63928e408122dda72f4d7895d2e9
mahditajdini <79911433+mahditajdini@users.noreply.github.com>
2023-08-21 19:27:06 +0330

    Update Constants.kt

--- app/src/main/java/com/the_tj/logoai/utils/Constants.kt
+++ app/src/main/java/com/the_tj/logoai/utils/Constants.kt
@@ -12,9 +12,9 @@ const val BASE_URL = "https://api.openai.com/v1/images/"
 const val NETWORK_TIMEOUT = 60L
 const val NAMED_HEADER = "named_header"
 const val NAMED_BODY = "named_body"
-const val APIKEY="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"
-//const val APIKEY="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"
-const val TEST_IMAGE="https://stimg.cardekho.com/images/carexteriorimages/930x620/Lamborghini/Urus/4418/Lamborghini-Urus-V8/1621927166506/front-left-side-47.jpg"
-const val TAPSELL_KEY="lqkmpipihbjhcnafotpqgnekqstkkcogcpiiegmdrlnqnlnkjssdeargrjcfkrbbdambqp"
-const val ZONE_ID_REWARDED_VIDEO="643d92ad57e0d3408b563615"
+const val APIKEY="
+
+const val TEST_IMAGE=
+const val TAPSELL_KEY
+const val ZONE_ID_REWARDED_VIDEO=
 
